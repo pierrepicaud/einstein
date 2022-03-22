e79199f5be49023191ea6c70c77bacc1bd474833
zZzwat4er <valenten2001@mail.ru>
2022-03-22 15:50:16 +0300

    Backend on firebace + dev-auth merge

--- lib/data/authentication/repos/account_database.dart
+++ lib/data/authentication/repos/account_database.dart
@@ -1,21 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:twitter_login/twitter_login.dart';
 
+class AccountDatabase {
+  final _auth = FirebaseAuth.instance;
 
-import 'package:einstein/data/authentication/modules/account.dart';
+  FirebaseAuth get auth => _auth;
 
-class AccountDatabase{
-
-  Future<bool> getAccount(Account account) async{
-    // TODO: check whether this acc exist in database
-    return true;
+  Future<UserCredential?> signInWithPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: "barry.allen@example.com", password: "SuperSecretPassword!");
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+    return null;
   }
 
-  Future<bool> addAccount(Account account) async{
-    // TODO: add account to database if we can
-    return true;
+  Future<UserCredential?> signUpWithPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: "barry.allen@example.com",
+              password: "SuperSecretPassword!");
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return null;
   }
 
-  void deleteAccount(Account account){
-    // TODO: delete acc from database
-  }
+  Future<UserCredential?> signINWithTwitter() async {
+    // Create a TwitterLogin instance
+    // TODO: Connect firebase API to this
+    final twitterLogin = TwitterLogin(
+        apiKey: '<your consumer key>',
+        apiSecretKey: ' <your consumer secret>',
+        redirectURI: '<your_scheme>://');
 
+    // Trigger the sign-in flow
+    final authResult = await twitterLogin.login();
+
+    // Create a credential from the access token
+    final twitterAuthCredential = TwitterAuthProvider.credential(
+      accessToken: authResult.authToken!,
+      secret: authResult.authTokenSecret!,
+    );
+
+    // Once signed in, return the UserCredential
+    return await _auth.signInWithCredential(twitterAuthCredential);
+  }
 }
