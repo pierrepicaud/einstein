2ce986d22f3bcb01ef4482a8d865cc05b51042f6
zZzwat4er <valenten2001@mail.ru>
2022-04-18 20:37:30 +0300

    DB for posts

--- lib/data/main_screen/repos/post_database.dart
+++ lib/data/main_screen/repos/post_database.dart
@@ -1,13 +1,48 @@
+import 'package:einstein/data/db_routs.dart';
 import 'package:einstein/data/main_screen/modules/post.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 
-class PostData{
+class PostsData extends ChangeNotifier {
+  DatabaseReference db = FirebaseDatabase.instance.ref();
+  Map<String, Post>? _posts;
+  Map<String, Post>? get posts => _posts;
 
-  List<Post> get posts => <Post>[
-    Post("Example text 1"),
-    Post("Example text 2"),
-    Post("Example text 3")
-  ];
+  PostsData() {
+    db
+        .child(DbRoutes.posts)
+        .orderByChild('date')
+        .limitToLast(10)
+        .onValue
+        .listen((event) {
+      final map = Map<String, dynamic>.from(event.snapshot.value as Map<dynamic, dynamic>);
+      _posts = map.map((key, value) => MapEntry(key, Post.fromMap(Map<String, dynamic>.from(value as Map<dynamic, dynamic>))),);
+      print(_posts);
+      notifyListeners();
+    });
+  }
 
-  
+  void _updateData(Map<String, Map?> updates) =>
+      db.update(updates).then((_) => notifyListeners());
 
+  void updatePost(String postID, Post post) async {
+    Map<String, Map> updates = {};
+    updates[DbRoutes.postData(postID)] = post.toMap();
+    return _updateData(updates);
+  }
+
+  void addPost(Post post) async {
+    Map<String, Map> updates = {};
+    final newPostKey = db.child(DbRoutes.posts).push().key;
+    // can shoot in a leg since I have no idea
+    // why new key is "String?"
+    updates[DbRoutes.postData(newPostKey!)] = post.toMap();
+    return _updateData(updates);
+  }
+
+  void deletePost(String postID) async {
+    Map<String, Map?> updates = {};
+    updates[DbRoutes.postData(postID)] = null;
+    return _updateData(updates);
+  }
 }
