b5b5f5606bead6a1abc1f2f36fcb519ed4ba8edc
zZzwat4er <valenten2001@mail.ru>
2022-05-05 17:22:35 +0300

    Card holder view done

--- lib/ui/home/card_holder.dart
+++ lib/ui/home/card_holder.dart
@@ -16,20 +16,22 @@ class CardHolder extends StatefulWidget {
 class _CardHolderState extends State<CardHolder> {
   @override
   Widget build(BuildContext context) {
+    // this keys are a kostil to say flutter that it needs to rebuild widgets in a Stack
+    Key k1 = UniqueKey();
+    Key k2 = UniqueKey();
     final snapshot =
         context.dependOnInheritedWidgetOfExactType<CardHolderInherit>()!;
     final post = snapshot.currentPost;
     final npost = snapshot.nextPost;
     return Scaffold(
-      body: Center(
-        //Container with widget
-        child: SizedBox(
-          height: 450,
-          width: 350,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          //Container with widget
           child: Stack(
             children: <Widget>[
-              TinderCard(color: Colors.orange, post: post),
-              TinderCard(color: Colors.orange, post: npost),
+              TinderCard(key: k1, color: Colors.orange, post: post),
+              TinderCard(key: k2, color: Colors.orange, post: npost),
             ],
           ),
         ),
