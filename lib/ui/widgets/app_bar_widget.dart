a7805fa8fe76d9eeac767901278aec2a8e9f266c
Pierre Picaud <pierrepicaud385@gmail.com>
2022-05-06 20:04:37 +0300

    Settings UI

--- lib/ui/widgets/app_bar_widget.dart
+++ lib/ui/widgets/app_bar_widget.dart
@@ -1,22 +1,27 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../settings_page.dart';
+
 AppBar buildAppBar(BuildContext context) {
-  final icon = CupertinoIcons.moon_stars;
+  // final icon = CupertinoIcons.moon_stars;
 
   return AppBar(
-    leading: BackButton(
+    leading: const BackButton(
       color: Colors.black,
     ),
     backgroundColor: Colors.transparent,
     elevation: 0,
     actions: [
       IconButton(
-          onPressed: () {},
-          icon: Icon(
-            icon,
-            color: Colors.black,
-          ))
+        icon: const Icon(
+          Icons.settings,
+          color: Colors.green,
+        ),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => SettingsPage()));
+        },
+      ),
     ],
   );
 }
